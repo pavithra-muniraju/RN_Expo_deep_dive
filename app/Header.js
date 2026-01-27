3f054033cbc03bdae88c743433128ff70a7fcd40
Pavithra Muniraju <pavithra.muniraju@ust.com>
2026-01-27 16:17:54 +0530

    assets added with header file

---
+++ app/Header.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import { AntDesign, Feather } from "@expo/vector-icons";
+const Header = () => {
+    const logo = require("../assets/bethanys-pie-shop-logo_horiz-white.png")
+    return(
+        <View style={styles.header}>
+            <Image source={logo} style={styles.logo}/>
+            <Text style={styles.menu}>SHOP</Text>
+            <Text style={styles.menu}>CONTACT</Text>
+            <Text style={styles.menu}>REGISTER</Text>
+            <AntDesign style={styles.menu} name='user' size={24} color='white' />
+            <Feather style={styles.menu} name='shopping-cart' size={24} color='white' />
+        </View>
+    )
+}
+export default Header;
+
+const styles = StyleSheet.create({
+    header: {
+        alignItems: "center",
+        justifyContent: 'center',
+        backgroundColor: '#d77948',
+        height: 80,
+        width: '100%',
+        flexDirection: 'row',
+    },
+    logo: {
+        width: 95,
+        height:30,
+        marginRight: 5,
+        marginLeft: 10
+    },
+    menu: {
+        paddingLeft: 8,
+        paddingRight: 8,
+        color: 'white',
+        fontWeight: 'bold'
+    }
+})
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import Header from './Header';
+const Home = () => {
+    return (
+        <View>
+            <Header />
+            <Text>Heelo from react</Text>
+        </View>
+    )
+}
+
+export default Home;
